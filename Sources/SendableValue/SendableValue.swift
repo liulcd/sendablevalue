34d202f64785db434861119f7775a3972476e32f
liulcd <liulcd@outlook.com>
2025-09-26 22:15:00 +0800

    Update code

--- Sources/SendableValue/SendableValue.swift
+++ Sources/SendableValue/SendableValue.swift
@@ -9,4 +9,27 @@ open class SendableValue: NSObject, @unchecked Sendable {
     init(value: Any?) {
         self.value = value
     }
+    
+    var anyHashableValue: AnyHashable? {
+        get {
+            return self.value as? AnyHashable
+        }
+    }
+    
+}
+
+open class SendableAnyValue: NSObject, @unchecked Sendable {
+    let value: Any
+    
+    init(value: Any) {
+        self.value = value
+    }
+}
+
+open class SendableAnyHashableValue: NSObject, @unchecked Sendable {
+    let anyHashableValue: AnyHashable
+    
+    init(anyHashableValue: AnyHashable) {
+        self.anyHashableValue = anyHashableValue
+    }
 }
